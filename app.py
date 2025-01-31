227925dadcf2078c301b57499231cb7e8e93ac10
amee342 <pt79493@gmail.com>
2025-01-31 17:03:50 +0100

    Added libs

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+from urllib.parse import urlparse
+
+import mlflow
+from mlflow.models.signature import infer_signature
+import mlflow.sklearn 
+
+import logging
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
